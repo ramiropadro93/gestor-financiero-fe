c671c42c1b6a65127d5d672ac449bb68e96540a9
RamiroPadro <ramirop@shifta.la>
2024-09-30 11:30:09 -0300

    Reporte mensual

--- pages/Reportes.tsx
+++ pages/Reportes.tsx
@@ -1,8 +1,26 @@
+import ProtectedRoute from '@/components/auth/ProtectedRoute';
+import { GetMovimientosByCategorias } from '@/services/MovimientoService';
+
 const Reportes = () => {
+    const getReporteMensual = async () => {
+        const response = await GetMovimientosByCategorias();
+        console.log('response', response);
+    };
+
     return (
-        <div>
-            <h1>Reportes</h1>
-        </div>
+        <ProtectedRoute>
+            <div className="flex flex-col items-center my-5">
+                <div className="my-5">
+                    <h1>Reportes</h1>
+                </div>
+                <div>
+                    <h2>Septiembre</h2>
+                    <button onClick={getReporteMensual}>
+                        Descargar reporte
+                    </button>
+                </div>
+            </div>
+        </ProtectedRoute>
     );
 };
 
--- services/MovimientoService.ts
+++ services/MovimientoService.ts
@@ -2,13 +2,19 @@ import { API_URL } from '@/constants/constants';
 import { getApi, postApi } from '@/helpers/ApiUtility';
 
 export const AddOrUpdateMovimiento = async (objeto: any) => {
-  const url = API_URL + '/create';
-  const response = await postApi(url, objeto);
-  return response;
+    const url = API_URL + '/create';
+    const response = await postApi(url, objeto);
+    return response;
 };
 
 export const GetMovimientos = async () => {
-  const url = API_URL + '/get';
-  const response = await getApi(url);
-  return response;
+    const url = API_URL + '/get';
+    const response = await getApi(url);
+    return response;
+};
+
+export const GetMovimientosByCategorias = async () => {
+    const url = API_URL + '/gastosPorCategoriaPDF';
+    const response = await getApi(url);
+    return response;
 };
